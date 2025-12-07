b9568ab0cad7ae2ad35b0f6347bd1de6253a3fac
AlexMShi <felixshi0216@gmail.com>
2025-12-07 15:10:08 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,8 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/emeis-trust-rebuild/",   
+
   server: {
     host: "::",
     port: 8080,
